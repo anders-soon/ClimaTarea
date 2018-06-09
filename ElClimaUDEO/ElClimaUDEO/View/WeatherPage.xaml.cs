c74b05447f61fd3ce0c94ba19911ba286ec1eedb
Andersson Hernandez <andersonemanuelhernandezsipaque@yahoo.es>
2018-06-09 00:13:57 -0500

    listo

---
+++ ElClimaUDEO/ElClimaUDEO/View/WeatherPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using ElClimaUDEO.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using System.Windows.Input;
+
+namespace ElClimaUDEO.View
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class WeatherPage : ContentPage
+	{
+		public WeatherPage ()
+		{
+			InitializeComponent ();
+            this.BindingContext = new WeatherViewModel();
+
+        }
+
+        
+
+        private void SearchBar_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+
+        }
+
+        private void SearchBar_TextChanged_1(object sender, TextChangedEventArgs e)
+        {
+            var oldText = e.OldTextValue;
+            var newText = e.NewTextValue;
+        }
+
+       
+    }
+}
